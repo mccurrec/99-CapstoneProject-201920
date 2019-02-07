6585fbde99f49ab3c3eb1f0bd8578695e7de01c4
strainlw <strainlw@rose-hulman.edu>
2019-02-07 16:40:46 -0500

    All arm handles implimented for testing

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -231,7 +231,10 @@ def handle_lower_arm(mqtt_sender):
     Tells the robot to lower its Arm until it is all the way down.
       :type  mqtt_sender:  com.MqttClient
     """
-    
+    print('Lower Arm')
+    mqtt_sender.send_message('lower_arm')
+
+
 
 
 def handle_calibrate_arm(mqtt_sender):
@@ -241,6 +244,8 @@ def handle_calibrate_arm(mqtt_sender):
     all the way down, and then to mark taht position as position 0.
       :type  mqtt_sender:  com.MqttClient
     """
+    print('Calibrate Arm')
+    mqtt_sender.send_message('calibrate_arm')
 
 
 def handle_move_arm_to_position(arm_position_entry, mqtt_sender):
@@ -250,6 +255,10 @@ def handle_move_arm_to_position(arm_position_entry, mqtt_sender):
       :type  arm_position_entry  ttk.Entry
       :type  mqtt_sender:        com.MqttClient
     """
+
+    print('Move arm to ', arm_position_entry)
+    mqtt_sender.send_message('move_arm_to_position', [arm_position_entry])
+
 
 
 ###############################################################################
