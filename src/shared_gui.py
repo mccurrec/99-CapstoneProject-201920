25a3eace2b7c9c8d4dbbbf2d6e63de4391ef2c8c
mccurrec <mccurrec@rose-hulman.edu>
2019-02-12 21:32:32 -0500

    removed the speed entry from the m1 feature handle

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -322,18 +322,14 @@ def get_m1_frame(window, mqtt_sender):
     frame = ttk.Frame(window, padding=10, borderwidth=5, relief='ridge')
 
     # LABELS:
-    frame_label = ttk.Label(frame, text='m1 Feature 9')
+    frame_label = ttk.Label(frame, text='m1 Feature 9', font=('bold'))
     frame_label.grid(row=0, column=0)
-    speed_label = ttk.Label(frame, text='Speed: ')
-    speed_label.grid(row=1, column=0)
     initial_label = ttk.Label(frame, text='Initial Time Between Beeps: ')
     initial_label.grid(row=2, column=0)
     decrease_label = ttk.Label(frame, text='Decrease Time Between Beeps By: ')
     decrease_label.grid(row=3, column=0)
 
     # ENTRY BOXES:
-    speed_entry = ttk.Entry(frame, width=8)
-    speed_entry.grid(row=1, column=1)
     initial_entry = ttk.Entry(frame, width=8)
     initial_entry.grid(row=2, column=1)
     decrease_entry = ttk.Entry(frame, width=8)
@@ -730,6 +726,7 @@ def handle_m2(m2_entry1, m2_entry2, mqtt_sender):
     print('got')
     mqtt_sender.send_message("m2", [m2_entry1.get(), m2_entry2.get()])
 
+
 def handle_m3_feature_9(initial_rate_entry, rate_of_increase_entry, mqtt_sender):
     """
     :type initial_rate_entry:  ttk.Entry
@@ -737,8 +734,8 @@ def handle_m3_feature_9(initial_rate_entry, rate_of_increase_entry, mqtt_sender)
     :type mqtt_sender: com.MqttClient
     """
     print('sending m3_feature9')
-    mqtt_sender.send_message('m3_feature_9', [float(initial_rate_entry.get()), float(rate_of_increase_entry.get()),
-                                             int(speed_entry.get())])
+    mqtt_sender.send_message('m3_feature_9', [float(initial_rate_entry.get()), float(rate_of_increase_entry.get())])
+
 
 def handle_m3_feature_10(speed_entry, direction_entry, mqtt_sender):
     """
